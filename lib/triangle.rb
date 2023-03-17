ecfcb286866bd683d5e2c33f5865dcad68fd4a03
Minchul An <min@minchulan.io>
2023-03-17 03:40:20 -0600

    commit tree 568ee9ffec3ae7de58b731f4e9e10a15b755e26c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end 
+
+
+  def kind(symbol_type)
+  end 
+
+  class TriangleError < StandardError
+    # triangle error code
+  end 
+
+
 end
